ed084de223b5bd14b63e35732fa026d1ec5abc41
blackongroup <info@blackongroup.com>
2025-05-24 12:28:33 -0400

    utils.py

--- model/utils.py
+++ model/utils.py
@@ -1,1 +1,14 @@
+# model/utils.py
 
+import json
+import numpy as np
+
+def save_params(params: np.ndarray, filename: str = "model/params.json"):
+    with open(filename, "w") as f:
+        json.dump(params.tolist(), f)
+
+
+def load_params(filename: str = "model/params.json") -> np.ndarray:
+    with open(filename, "r") as f:
+        data = json.load(f)
+    return np.array(data)
